b4df9eff50124aa4de28b3531febb4750b67340f
johnrbell <johnrbell@gmail.com>
2023-08-13 00:39:24 -0400

    push full project before big changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
+#include "FastAccelStepper.h"
+#include <HardwareSerial.h>
+#include <TMCStepper.h>
+#include <SPI.h>
 
+#define LED_PIN 2
 
-const int dirPin = 14;
-const int stepPin = 15;
-const int enPin = 27;
+const int enPin = 12;
+const int stepPin = 14;
+const int dirPin = 27;
 
-const int limit_ext = 25; //extend limit -  0 == short to ground
-const int limit_ret = 33; //retract limit -  0 == short to ground
+const int limit_ext = 22; //extend limit -  0 == short to ground
+const int limit_ret = 23; //retract limit -  0 == short to ground
 
 int direction = 1; // 0 = extend
 
@@ -15,52 +20,34 @@ void setup()
   Serial.begin(115200); delay(1000);
   Serial.println("welcome to flavortown. 🍌");
 
+  pinMode(LED_PIN, OUTPUT);
+
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(enPin, OUTPUT);
-  pinMode(limit_ext, INPUT_PULLUP); //watch for limit shorting to ground
-  pinMode(limit_ret, INPUT_PULLUP); //watch for limit shorting to ground
+  pinMode(limit_ext, INPUT_PULLUP); //watch for limit shorting to ground (blk/wht for extend limit)
+  pinMode(limit_ret, INPUT_PULLUP); //watch for limit shorting to ground (red/blu for retract limit)
 
   digitalWrite(enPin, LOW);
   digitalWrite(dirPin, direction);
 }
 
-void readLimits(){
-  Serial.println("-");
-  Serial.println(digitalRead(limit_ext));
-  Serial.println(digitalRead(limit_ret));
-}
-
 void loop()
 {
-  // Serial.println("--------------");
-  // Serial.print("Limit A Val: ");
-  // Serial.println(digitalRead(limit_ext));
-  // Serial.print("Limit B Val: ");
-  // Serial.println(digitalRead(limit_ret));
-
-  //send a... few pulses. 
-  // for (int i=0; i<50000; i++){  
-    // if(i % 100 == 0)
-    // {
-    //      Serial.println(i);
-    // }    
-
-
-    
+  
     //actual pulse. 
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(75);
     digitalWrite(stepPin, LOW);
     delayMicroseconds(75);    
-  // }
+  
     if (digitalRead(limit_ext)==0){       //if extend limit is hit
       Serial.print("Extend Limit Hit");
-      delayMicroseconds(1000);
+      delayMicroseconds(4000);
       digitalWrite(dirPin, 1);          //retract  
     } else if(digitalRead(limit_ret)==0){ //if retract limit is hit
       Serial.print("Retract Limit Hit");
-      delayMicroseconds(1000);
+      delayMicroseconds(4000);
       digitalWrite(dirPin, 0);          //extend
     }   
 
